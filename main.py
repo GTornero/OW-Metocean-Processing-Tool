5fcc55af0cd998636839b4fe309b6dd497f9f5d6
Guillermo Tornero <guillermo.tornero@oceanwinds.com>
2021-01-12 15:31:52 +0000

    Initial version of the main script.

---
+++ main.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import filedialog
+import time
+
+import xlsxwriter
+
+from metocean_data import MetoceanData
+
+# from scatter import Scatter    # dont need this
+from scatter_report import print_scatter_report
+from NSS import NSS
+
+
+def main():
+    # Inisialise the tkinter interface
+    root = tk.Tk()
+    root.iconbitmap("OW_logo.ico")
+    # Asks the user to select the config file and stores the full path.
+    config_filepath = filedialog.askopenfilename(
+        title="Select the metocean configuration file.",
+        filetypes=[("Excel Files", "*.xlsx")],
+    )
+    # Create the MetoceanData oject that will hold all of the data and configuration setup.
+    metocean_data = MetoceanData(config_filepath)
+    print(metocean_data.data.head())
+
+    # ---------------------------------------------------------------------------------------------
+    # -----------------------------------Creating the NSS Table Report-----------------------------
+    # ---------------------------------------------------------------------------------------------
+
+    # Method for taking mean or median within bin to be implemented
+    # if metocean_data.config["wind_status"] & metocean_data.config["wave_status"]:
+    #     NSS_tables = NSS.NSS(metocean_data)
+
+    # ---------------------------------------------------------------------------------------------
+    # ---------------------------------Creating the Scatter Table Report---------------------------
+    # ---------------------------------------------------------------------------------------------
+    print_scatter_report(metocean_data)
+
+
+if __name__ == "__main__":
+    main()
